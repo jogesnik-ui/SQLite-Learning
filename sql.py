15e6efea305e39a5258d1cb6ccc0814bfaf4820a
DominikKutyna <DJ.Kutyna@outlook.com>
2026-03-15 17:45:51 +1030

    Added functions for easier accesibility, addtasks, complete task, get tasks

--- sql.py
+++ sql.py
@@ -12,21 +12,46 @@ CREATE TABLE IF NOT EXISTS tasks (
                done INTEGER)
 """)
 
+def add_task(cursor, subject, description, due_date, done):
+    cursor.execute(
+        "INSERT INTO tasks (subject, description, due_date, done) VALUES(?, ?, ?, ?)",
+            (subject, description, due_date, 0))
+    
+def complete_task(cursor, conn, subject):
+    cursor.execute("UPDATE tasks SET done = ? WHERE subject =?",
+                   ( 1, subject))
+    conn.commit()
 
-cursor.execute("INSERT INTO tasks (subject, description, due_date, done) VALUES(?, ?, ?, ?)",
-               ("Maths","All of 1J", "Monday", 0))
 
+def get_tasks(conn, cursor, subject):
+    cursor.execute("SELECT * FROM tasks")
+    rows = cursor.fetchall()
+    print (rows)
+              
+               
+#Add Tasks  
+add_task(cursor, "Maths","All of 1J", "Monday", 0)
+add_task(cursor, "English", "Essay draft", "Tuesday", 0)
+add_task(cursor, "Science", "Lab report", "Wednesday", 0)
 
-
-cursor.execute("UPDATE tasks SET done = ? WHERE subject =?",
-               (1, "Maths")
+cursor.execute("DELETE FROM tasks WHERE subject=?",
+               ("Maths",)
                )
 
-cursor.execute("SELECT * FROM tasks")
-rows = cursor.fetchall() 
-print(rows)
-               
+
+
+#Mark English done
+complete_task(cursor, conn, "English")
+
+#Get all tasks
+get_tasks(conn, cursor, "subject")
+
 
 conn.commit()
+ 
+
+
 conn.close()
-print("Table created")
+print("Table created!")
+
+
